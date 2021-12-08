3e0615fe287b1e21b3c9372026aed2c3877dafcc
Brayan-PM <2995454660101@ingenieria.usac.edu.gt>
2021-12-08 02:09:11 -0600

    Prueba

---
+++ JS/ERRORES/N_Errores.js
@@ -0,0 +1,10 @@
+class N_Errores{
+    constructor(tipo, descripcion, fila, columna){
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.fila = fila;
+        this.columna = columna;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
